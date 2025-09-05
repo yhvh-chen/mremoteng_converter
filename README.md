f7001da768363ad03a945f6c37169f3d4f4643a9
yhvh-chen <73627640+yhvh-chen@users.noreply.github.com>
2025-09-05 13:51:32 +1000

    Initial commit of mremoteng_converter project with converter.ps1 and README.md.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # mremoteng_converter
-A PowerShell script to convert CSV session data to mRemoteNG XML format.
+
+A PowerShell script to convert session data from CSV to mRemoteNG XML format.
+
+## Usage
+
+1. Prepare a `sessions.csv` file with the following headers:
+   - Name
+   - Hostname
+   - Protocol
+   - Container
+   - Description
+2. Run the script:
+   ```powershell
+   .\converter.ps1
+   ```
+3. The script will generate a `sessions.xml` file for import into mRemoteNG.
---
+++ converter.ps1
@@ -0,0 +1,83 @@
+try {
+    # Import the connection data from your CSV file.
+    # Ensure your CSV file has the headers: Name,Hostname,Protocol,Container,Description
+    $Content = Import-Csv 'sessions.csv'
+
+    # Create a new XML document
+    $Xml = New-Object -TypeName System.Xml.XmlDocument
+
+    # Create the XML declaration
+    $xmlDeclaration = $Xml.CreateXmlDeclaration("1.0", "utf-8", $null)
+    $Xml.AppendChild($xmlDeclaration)
+
+    # Create a namespace manager
+    $nsmgr = New-Object -TypeName System.Xml.XmlNamespaceManager -ArgumentList $Xml.NameTable
+    $nsmgr.AddNamespace("mrng", "http://mremoteng.org")
+
+    # Create the root node with the correct namespace
+    $RootNode = $Xml.CreateElement("mrng", "Connections", "http://mremoteng.org")
+    $RootNode.SetAttribute("Name", "New Folder")
+    $RootNode.SetAttribute("Export", "false")
+    $RootNode.SetAttribute("EncryptionEngine", "AES")
+    $RootNode.SetAttribute("BlockCipherMode", "GCM")
+    $RootNode.SetAttribute("KdfIterations", "10000")
+    $RootNode.SetAttribute("FullFileEncryption", "false")
+    $RootNode.SetAttribute("Protected", "1QHA5bWmnsqjypq/xw3J71N1UPkehZ/Ra6ITcNprbGMLffH+79wfdcH4YWa0xYuNUSSZwzs8VQ41kRy24eDiA2oT")
+    $RootNode.SetAttribute("ConfVersion", "2.8")
+    $Xml.AppendChild($RootNode)
+
+    function Create-Container($parentNode, $containerPath) {
+        $pathParts = $containerPath.Split('\')
+        $currentNode = $parentNode
+        foreach ($part in $pathParts) {
+            $childNode = $currentNode.SelectSingleNode("mrng:Node[@Name='$part']", $nsmgr)
+            if (-not $childNode) {
+                $childNode = $Xml.CreateElement("mrng", "Node", "http://mremoteng.org")
+                $childNode.SetAttribute("Name", $part)
+                $childNode.SetAttribute("Type", "Container")
+                $childNode.SetAttribute("Expanded", "False")
+                $childNode.SetAttribute("Id", [System.Guid]::NewGuid().ToString())
+                $currentNode.AppendChild($childNode)
+            }
+            $currentNode = $childNode
+        }
+        return $currentNode
+    }
+
+    # This will loop through each row in your CSV file and create a connection object for it.
+    foreach ($entry in $Content) {
+        # Create the container structure
+        $Container = Create-Container -parentNode $RootNode -containerPath $entry.Container
+
+        # Create a new connection and add it to the container
+        $Connection = $Xml.CreateElement("mrng", "Node", "http://mremoteng.org")
+        $Connection.SetAttribute("Name", $entry.Name)
+        $Connection.SetAttribute("Type", "Connection")
+        $Connection.SetAttribute("Id", [System.Guid]::NewGuid().ToString())
+        $Connection.SetAttribute("Hostname", $entry.Hostname)
+        $Connection.SetAttribute("Protocol", $entry.Protocol)
+        $Connection.SetAttribute("Description", $entry.Description)
+        $Connection.SetAttribute("InheritCredential", "True")
+        $Connection.SetAttribute("PuttySession", "Cisco-SSH")
+        $Container.AppendChild($Connection)
+    }
+
+    # Save the XML to a file
+    $Xml.Save("sessions.xml")
+
+    # Provide feedback that the script has completed successfully.
+    Write-Host "Successfully created 'sessions.xml'. You can now import this file into mRemoteNG."
+
+}
+catch {
+    # If any error occurs in the 'try' block, this 'catch' block will execute.
+    Write-Host "An error occurred:" -ForegroundColor Red
+    # The $_ variable holds the details of the specific error that was caught.
+    Write-Host $_.Exception.Message
+    Write-Host "Script execution halted." -ForegroundColor Yellow
+}
+finally {
+    # This block will run regardless of whether there was an error or not.
+    # It pauses the script, keeping the window open so you can read any output or error messages.
+    Read-Host -Prompt "Press Enter to exit"
+}
